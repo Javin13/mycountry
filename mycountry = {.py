339bd7a45f571ace185c1547fd3f5041fbd7d0ed
Javin13 <reachjavin@gmail.com>
2024-11-23 12:40:11 +0000

    Add files via upload

---
+++ mycountry = {.py
@@ -0,0 +1,20 @@
+mycountry = {
+    "Name" : "Javin",
+    "Continent" : "Europe",
+    "Union" : "United Kingdom",
+    "Country" : "Great England",
+    "Capital" : "London",
+    "City" : "London",
+    "Town" : "Maidstone",
+    "Constituency" : "Maidstone and The Weald",
+    "Economical Growth" : "Good",
+    "Industrialised?" : "Yes",
+}
+
+print(mycountry)
+
+del(mycountry["City"])
+
+mycountry["Political Status"] = "Excellent"
+
+print(mycountry)
